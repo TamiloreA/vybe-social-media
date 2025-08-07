9873c15212f79e6eb77275db079ca36010c9a17c
TamiloreA <135843913+TamiloreA@users.noreply.github.com>
2025-08-07 22:14:55 +0100

    Update main-app.tsx

--- src/app/components/main-app.tsx
+++ src/app/components/main-app.tsx
@@ -359,7 +359,7 @@ export default function MainApp({ onSignOut }: MainAppProps) {
       return;
     }
 
-    const newSocket = io("http://localhost:5001", {
+    const newSocket = io("https://vybe-social-backend.onrender.com", {
       path: "/socket.io",
       auth: {
         token: localStorage.getItem("authToken"),
